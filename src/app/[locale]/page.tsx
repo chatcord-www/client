5d83b0289355a9a21d9403766ac79a8cffcc8fa9
formyauto75@gmail.com <formyauto75@gmail.com>
2024-05-30 17:08:40 +0400

    feat: cta section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,67 +1,36 @@
-import { Link } from "@/navigation";
-import { getTranslations } from "next-intl/server";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const t = await getTranslations();
-
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
-      <div className="relative overflow-hidden">
-        <div
-          aria-hidden="true"
-          className="absolute -top-96 start-1/2 flex -translate-x-1/2 transform"
-        >
-          <div className="h-[44rem] w-[25rem] -translate-x-[10rem] rotate-[-60deg] transform bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl dark:from-violet-900/50 dark:to-purple-900"></div>
-          <div className="rounded-fulls h-[50rem] w-[90rem] origin-top-left -translate-x-[15rem] -rotate-12 bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl dark:from-indigo-900/70 dark:via-indigo-900/70 dark:to-blue-900/70"></div>
-        </div>
-        <div className="relative z-10">
-          <div className="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-16">
-            <div className="mx-auto max-w-2xl text-center">
-              <p className="inline-block bg-gradient-to-l from-blue-600 to-violet-500 bg-clip-text text-sm font-medium text-transparent dark:from-blue-400 dark:to-violet-400">
-                Preline: A vision for 2023
+    <main className="relative flex min-h-screen items-center justify-center overflow-hidden">
+      <div className="absolute -top-96 start-1/2 flex -translate-x-1/2 transform">
+        <div className="h-[44rem] w-[25rem] -translate-x-[10rem] rotate-[-60deg] transform bg-gradient-to-r from-violet-300/50 to-purple-100 blur-3xl dark:from-green-900/50 dark:to-green-900" />
+        <div className="rounded-fulls h-[50rem] w-[90rem] origin-top-left -translate-x-[15rem] -rotate-12 bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl dark:from-green-900/70 dark:via-emerald-900/70 dark:to-green-900/70" />
+      </div>
+      <div className="relative z-10">
+        <div className="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-16">
+          <div className="mx-auto max-w-2xl text-center">
+            <p className="inline-block bg-gradient-to-l from-green-400 to-green-700 bg-clip-text text-sm font-medium text-transparent dark:from-emerald-500 dark:to-emerald-400">
+              Preline: A vision for 2024
+            </p>
+            <div className="mt-5 max-w-2xl">
+              <h1 className="block text-4xl font-semibold text-gray-800 dark:text-neutral-200 md:text-5xl lg:text-6xl">
+                Web Chatting Aplication
+              </h1>
+            </div>
+
+            <div className="mt-5 max-w-3xl">
+              <p className="text-lg text-gray-600 dark:text-neutral-400">
+                Our chatting application is built with Drizzle ORM, Next.js,
+                Tailwind CSS, and ShadCN UI, providing a seamless communication
+                experience with a modern interface and efficient performance.
               </p>
-              <div className="mt-5 max-w-2xl">
-                <h1 className="block text-4xl font-semibold text-gray-800 dark:text-neutral-200 md:text-5xl lg:text-6xl">
-                  The Intuitive Web Solutions
-                </h1>
-              </div>
-
-              <div className="mt-5 max-w-3xl">
-                <p className="text-lg text-gray-600 dark:text-neutral-400">
-                  Preline UI is an open-source set of prebuilt UI components,
-                  ready-to-use examples and Figma design system based on the
-                  utility-first Tailwind CSS framework.
-                </p>
-              </div>
-
-              <div className="mt-8 flex justify-center gap-3">
-                <a
-                  className="inline-flex items-center gap-x-2 rounded-lg border border-transparent bg-blue-600 px-4 py-3 text-sm font-semibold text-white hover:bg-blue-700 disabled:pointer-events-none disabled:opacity-50"
-                  href="#"
-                >
-                  Get started
-                  <svg
-                    className="size-4 flex-shrink-0"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="m9 18 6-6-6-6" />
-                  </svg>
-                </a>
-                <Link
-                  className="inline-flex items-center gap-x-2 rounded-lg border border-transparent px-4 py-3 text-sm font-semibold text-gray-800 hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50 dark:text-white dark:hover:bg-neutral-800"
-                  href="#"
-                >
-                  Preline Figma
-                </Link>
-              </div>
+            </div>
+            <div className="mt-3 flex justify-center gap-3">
+              <Button size="lg">Get started</Button>
+              <Button size="lg" variant="link">
+                Continue with discord
+              </Button>
             </div>
           </div>
         </div>
