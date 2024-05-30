846ac0e46274c374cff31008bc11f8a131aae620
formyauto75@gmail.com <formyauto75@gmail.com>
2024-05-30 16:46:16 +0400

    fix: linter issue

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,11 +1,8 @@
 import { publicProcedure } from "@/server/api/trpc";
 import { z } from "zod";
-import { users } from "@/server/db/schema";
 
-export const postRouter = publicProcedure
-  .input(
-    z.object({
-      name: z.string(),
-    }),
-  )
-  .mutation(async ({ input, ctx }) => {});
+export const postRouter = publicProcedure.input(
+  z.object({
+    name: z.string(),
+  }),
+);
