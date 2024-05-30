c12a3941b1e372389e92137f8e646769640f0119
xhxe <65135792+xhxe@users.noreply.github.com>
2024-05-30 16:44:05 +0400

    nothign important (#1)

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,3 +1,11 @@
-import { createTRPCRouter } from "@/server/api/trpc";
+import { publicProcedure } from "@/server/api/trpc";
+import { z } from "zod";
+import { users } from "@/server/db/schema";
 
-export const postRouter = createTRPCRouter({});
+export const postRouter = publicProcedure
+  .input(
+    z.object({
+      name: z.string(),
+    }),
+  )
+  .mutation(async ({ input, ctx }) => {});
