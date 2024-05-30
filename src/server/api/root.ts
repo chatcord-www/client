498580dcb5c8514f5e8e801d258a65cbe186333c
formyauto75@gmail.com <formyauto75@gmail.com>
2024-05-30 16:48:33 +0400

    fix: linter issue

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,6 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 
-export const appRouter = createTRPCRouter({
-  post: postRouter,
-});
+export const appRouter = createTRPCRouter({});
 
 export type AppRouter = typeof appRouter;
 export const createCaller = createCallerFactory(appRouter);
