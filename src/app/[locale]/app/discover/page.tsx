bae542cccdfc33e27de4572c0acc1ed2b5476428
formyauto75@gmail.com <formyauto75@gmail.com>
2024-06-02 18:34:24 +0400

    feat: discovery banner

--- src/app/[locale]/app/discover/page.tsx
+++ src/app/[locale]/app/discover/page.tsx
@@ -1,5 +1,25 @@
-export default function DiscoverPage() {
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import { getTranslations } from "next-intl/server";
+
+export default async function DiscoverPage() {
+  const t = await getTranslations("discover");
+
   return (
-    <div>Discover page</div>
-  )
+    <header className="absolute top-0">
+      <div className="h-40 w-full overflow-hidden">
+        <img src="/assets/discovery-background.svg" alt="banner" draggable={false} />
+      </div>
+      <div className="px-3">
+        <h1 className="mt-2 text-2xl">{t("title")}</h1>
+        <div className="flex items-center">
+          <Input
+            className="mt-1 max-w-96 pr-8"
+            placeholder={t("input-placeholer")}
+          />
+          <Search size={16} className="text-zinc-400 -translate-x-7 mt-1"/>
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/pages/global/sidebar/avatar-icon.tsx
+++ src/components/pages/global/sidebar/avatar-icon.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 export const AvatarIcon = () => {
   return (
-    <Avatar className="cursor-pointer">
+    <Avatar className="curso">
       <AvatarImage src="https://cdn.discordapp.com/avatars/1026793851685437491/3f3a7a8ae1c6d03986a03564863e2d9b.png" />
       <AvatarFallback>N</AvatarFallback>
     </Avatar>
