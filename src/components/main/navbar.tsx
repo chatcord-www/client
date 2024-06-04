d34f647f17126ed0ea7a796614568b6f5f9cb54b
formyauto75@gmail.com <formyauto75@gmail.com>
2024-06-04 21:57:54 +0400

    fix: navbar padding fix

--- src/components/main/navbar.tsx
+++ src/components/main/navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="fixed left-0 top-0 z-50 w-full p-4">
+    <nav className="fixed left-0 top-0 z-50 w-full py-4">
       <div className="container mx-auto flex items-center justify-between">
         <Link href="/" className="flex items-center">
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
