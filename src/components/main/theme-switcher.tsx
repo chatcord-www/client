1dcf304391aeac9b287201ea4921e8b0876de86f
Turbovv <108888834+Turbovv@users.noreply.github.com>
2024-06-04 18:09:46 +0400

    fix: navbar (#4)

--- src/components/main/theme-switcher.tsx
+++ src/components/main/theme-switcher.tsx
@@ -1,14 +1,15 @@
-"use client";
-import { MoonIcon } from "lucide-react";
-import { useTheme } from "next-themes";
+'use client';
+
+import React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuTrigger,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Button } from "../ui/button";
 
 const ThemeSwitcher: React.FC = () => {
   const { theme, setTheme } = useTheme();
@@ -20,13 +21,19 @@ const ThemeSwitcher: React.FC = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button size='icon' variant='ghost'>
-          <MoonIcon className="h-6 w-6 dark:text-white" />
-        </Button>
+        <button className="flex items-center justify-center">
+          {theme === 'light' ? (
+            <Sun />
+          ) : (
+            <Moon />
+          )}
+        </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
         <DropdownMenuRadioGroup value={theme} onValueChange={handleThemeChange}>
-          <DropdownMenuRadioItem value="dark">Dark Theme</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark">
+            Dark Theme
+          </DropdownMenuRadioItem>
           <DropdownMenuRadioItem value="light">
             Light Theme
           </DropdownMenuRadioItem>
