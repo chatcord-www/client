50455481ce70d899c7640b5124a27705c4491d6b
Turbovv <108888834+Turbovv@users.noreply.github.com>
2024-06-08 23:01:15 +0400

    feat: next auth (#6)
    * feat: next auth
    
    * fix: qick fixes
    
    ---------
    
    Co-authored-by: formyauto75@gmail.com <formyauto75@gmail.com>

---
+++ src/components/main/sign.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
+export default async function DiscordSignInButton({
+  buttonText,
+}: {
+  buttonText: string;
+}) {
+  const handleDiscordSignIn = async () => {
+    await signIn("discord");
+  };
+
+  return (
+    <Button variant={"link"} onClick={handleDiscordSignIn}>
+      {buttonText}
+    </Button>
+  );
+}
