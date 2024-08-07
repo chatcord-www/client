d7a57173cc88c8dab8f72c0f3db8f378ab8368f6
formyauto75@gmail.com <formyauto75@gmail.com>
2024-08-08 00:52:14 +0400

    fix

--- src/components/ui/pogressbar.tsx
+++ src/components/ui/pogressbar.tsx
@@ -4,6 +4,7 @@ import { Next13ProgressBar } from "next13-progressbar";
 export const Pogressbar = () => {
   return (
     <Next13ProgressBar
+      className="z-50"
       height="1px"
       color="hsl(155 99% 45%)"
       options={{ showSpinner: false }}
